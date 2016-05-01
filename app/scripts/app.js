88883e8fc1ee7b6c1adb862995d1a3858dfb290c
Waverley Design <charles@waverleydesigncompany.com>
2016-05-02 08:28:23 +1000

    complete rejig of system

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,8 +15,8 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
 
-  app.firebaseURL = 'https://polymer-todo.firebaseio.com';
-  
+  app.firebaseURL = 'https://waverleydash.firebaseio.com';
+
   app.signOut = function() {
     this.$.auth.signOut();
   };
